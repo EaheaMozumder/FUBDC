34858bcf458b79f90ac5c20a0fb2f4563570b490
Eahea Mozumder <112267087+EaheaMozumder@users.noreply.github.com>
2022-12-22 21:02:16 +0600

    Update script.js

--- script.js
+++ script.js
@@ -77,24 +77,23 @@ $(document).ready(function(){
             }
         }
     });
-});
 
 // testimonial slide
-let userTexts = document.getElementsByClassName("user-text");
-let userPics = document.getElementsByClassName("user-pic");
-
-function showReview(){
-    for(userPic of userPics){
-        userPic.classList.remove("active-pic");
-    }
-    for(userText of userTexts){
-        userText.classList.remove("active-text");
-    }
-    let i = Array.from(userPics).indexOf(event.target);
-  
-    userPics[i].classList.add("active-pic");
-    userTexts[i].classList.add("active-text");
-}
+    $("#testimonial-slider").owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+        }
+    });
 
 
 
+    
+  });
